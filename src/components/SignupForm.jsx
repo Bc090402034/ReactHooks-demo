ca1e0b31c8f769178c96794e7e5f7b291989c6bc
Bc090402034 <arain.asd@gmail.com>
2025-06-27 13:22:13 +0500

    update in signup form
    update in signup form

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -7,6 +7,7 @@ function SignupForm() {
   const [gender, setGender] = useState('');
   const [hobbies, setHobbies] = useState([]);
   const [aboutMe, setAboutMe] = useState('');
+  const [province, setProvince] = useState ('')
 
   // check which checkbox  is checked or not also which changes
   function handleHobbyChange(e) {
@@ -27,6 +28,7 @@ function SignupForm() {
       gender,
       hobbies,
       aboutMe,
+      province,
     };
     console.log('Submitted Data:', formData);
     alert("Form submitted! Check console.");
@@ -36,23 +38,33 @@ function SignupForm() {
     <form onSubmit={handleSubmit} style={{ padding: '20px', maxWidth: '500px' }}>
       <h2>Signup Form (Multiple useState)</h2>
 
-      <label>Full Name:</label><br />
+      <label>Full Name:</label>
       <input type="text" value={fullName} onChange={(e) => setFullName(e.target.value)} /><br /><br />
 
-      <label>Email:</label><br />
+      <label>Email:</label>
       <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} /><br /><br />
 
-      <label>Password:</label><br />
+      <label>Password:</label>
       <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} /><br /><br />
 
-      <label>Gender:</label><br />
+      <label>Gender:</label>
       <label><input type="radio" value="Male" checked={gender === 'Male'} onChange={(e) => setGender(e.target.value)} /> Male</label>
       <label><input type="radio" value="Female" checked={gender === 'Female'} onChange={(e) => setGender(e.target.value)} /> Female</label><br /><br />
 
-      <label>Hobbies:</label><br />
+      <label>Hobbies:</label>
       <label><input type="checkbox" value="Cricket" checked={hobbies.includes('Cricket')} onChange={handleHobbyChange} /> Cricket</label>
       <label><input type="checkbox" value="Hockey" checked={hobbies.includes('Hockey')} onChange={handleHobbyChange} /> Hockey</label>
       <label><input type="checkbox" value="Football" checked={hobbies.includes('Football')} onChange={handleHobbyChange} /> Football</label><br /><br />
+
+      <label>Province:</label>
+      <select value={province} onChange={(e) => setProvince(e.target.value)}>
+        <option value="">-- Select Province --</option>
+        <option value="Punjab">Punjab</option>
+        <option value="Sindh">Sindh</option>
+        <option value="Balochistan">Balochistan</option>
+        <option value="KPK">KPK</option>
+        <option value="Gilgit Baltistan">Gilgit Baltistan</option>
+      </select><br /><br />
 
       <label>About Me:</label><br />
       <textarea value={aboutMe} onChange={(e) => setAboutMe(e.target.value)} rows="4" cols="40" /><br /><br />
@@ -61,7 +73,7 @@ function SignupForm() {
 
       <hr />
       <h3>Live Data:</h3>
-      <pre>{JSON.stringify({ fullName, email, password, gender, hobbies, aboutMe }, null, 2)}</pre>
+      <pre>{JSON.stringify({ fullName, email, password, gender, hobbies, aboutMe, province }, null, 2)}</pre>
     </form>
   );
 }
